06455a150e78068b9f79eb5d7027dc41b7888639
muhammadImtiaz27 <123411742+muhammadImtiaz27@users.noreply.github.com>
2024-10-12 20:10:51 +0800

    Update script.js
    Check if the user's browser support Geolocation.
    
    If so, attempt to get their current location.
    If successful, display a message.
    Otherwise, display another message.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let x = 5;
+const details = document.querySelector('.details');
+
+// Check if navigator exists (older browsers don't support navigator)
+// prettier-ignore
+if (navigator.geolocation) {
+    // If navigator exists, attempt to get user's current location.
+    navigator.geolocation.getCurrentPosition(function(){alert("Current location found!")}, function(){alert("Could not get your position")});
+} 
+else {
+    alert('Geolocation is not supported by this browser.');
+}
